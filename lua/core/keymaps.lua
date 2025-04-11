8a41d48594a8284acd01e90b07e0cb6b63d96ed9
Holler János <hollerjanika@gmail.com>
2025-04-11 13:54:28 +0200

    Keymap updates
    Added Insert mode SHIFT-TAB indentation.
    
    Added Visual mode SHIFT-TAB, TAB indentations.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,9 @@
 vim.g.mapleader = " "
 
+vim.keymap.set("i", "<S-Tab>", "<C-d>")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
